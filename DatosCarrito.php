46f8cbdb9807cd08661f260b54cfffaf966c7aa5
mmesa269 <110608005+mmesa269@users.noreply.github.com>
2023-09-03 19:28:32 -0500

    Update DatosCarrito.php
    Get cost no eran necesarios

--- DatosCarrito.php
+++ DatosCarrito.php
@@ -1,27 +1,15 @@
 <?php
-
 class contadores
 {
     private $cont1;
-    private $cost1;
     private $cont2;
-    private $cost2;
     private $cont3;
-    private $cost3;
 
-    private $nombre;
-    private $edad;
-    private $genero;
-
-
-    public function __construct($cont1, $cost1, $cont2, $cost2, $cont3, $cost3)
+    public function __construct($cont1, $cont2, $cont3)
     {
         $this->cont1 = $cont1;
-        $this->cost1 = $cost1;
         $this->cont2 = $cont2;
-        $this->cost2 = $cost2;
         $this->cont3 = $cont3;
-        $this->cost3 = $cost3;
     }
 
     public function getcont1()
@@ -36,18 +24,6 @@ class contadores
         return $this;
     }
 
-    public function getcost1()
-    {
-        return $this->cost1;
-    }
-
-
-    public function setcost1($cost1)
-    {
-        $this->cost1 = $cost1;
-
-        return $this;
-    }
     public function getcont2()
     {
         return $this->cont2;
@@ -60,18 +36,6 @@ class contadores
         return $this;
     }
 
-    public function getcost2()
-    {
-        return $this->cost2;
-    }
-
-
-    public function setcost2($cost2)
-    {
-        $this->cost2 = $cost2;
-
-        return $this;
-    }
     public function getcont3()
     {
         return $this->cont3;
@@ -80,19 +44,6 @@ class contadores
     public function setcont3($cont3)
     {
         $this->cont3 = $cont3;
-
-        return $this;
-    }
-
-    public function getcost3()
-    {
-        return $this->cost3;
-    }
-
-
-    public function setcost3($cost3)
-    {
-        $this->cost3 = $cost3;
 
         return $this;
     }
